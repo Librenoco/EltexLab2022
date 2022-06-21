502e5582ba99622778e7a5174e05ab13e1e0aba9
Warnet <warnet1920@gmail.com>
2022-06-21 22:58:13 +0700

    HomeWork2(task3)

---
+++ HomeWork2/task3/task3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int IsPassOk(void);
+
+int main(void)
+{
+    int PwStatus;
+    printf("\nMain = %p  IsPassOk = %p\n", main, IsPassOk);
+    puts("Enter password:");
+    PwStatus = IsPassOk();
+
+    if (PwStatus == 0)
+    {
+        printf("Bad password!\n");
+        exit(1);
+    }
+    else
+    {
+        printf("Access granted!\n");
+    }
+
+    return 0;
+}
+
+int IsPassOk(void)
+{
+    char Pass[12];
+
+    gets(Pass);
+
+    return 0 == strcmp(Pass, "test");
+}

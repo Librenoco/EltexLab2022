dcc2b136c7df66e0519b860e5d83e5024cf18d48
Warnet <warnet1920@gmail.com>
2022-07-04 22:34:55 +0700

    reload Homework

--- HomeWork3/task3/calculator.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALCULATOR_H
-#define CALCULATOR_H
-
-int addition (int, int);
-int subtraction (int, int);
-int division (int, int);
-int multiplication (int, int);
-
-#endif /*CALCULATOR_H*/

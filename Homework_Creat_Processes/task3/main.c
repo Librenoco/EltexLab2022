4b0582c4dd7c02c6220f05c4bde11f01ac84edd0
Warnet <warnet1920@gmail.com>
2022-07-08 19:59:15 +0700

    Creating processes tree (fork)

---
+++ Homework_Creat_Processes/task3/main.c
@@ -0,0 +1,12 @@
+// Интерпретатор командной строки (исполняемый файл с аргументами)
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void main(void) {
+
+  exit(EXIT_SUCCESS);
+}
